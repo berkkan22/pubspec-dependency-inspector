c5af627876fc0e48c98600c4852aa9fac54ca8e0
Berkkan <berkkan22@gmail.com>
2023-06-22 13:35:19 +0200

    create files and add body of commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pubspec-dependency-inspector" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pubspec-dependency-inspector.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Pubspec dependency inspector!');
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pubspec-dependency-inspector.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from Pubspec dependency inspector!');
+		}),
 
-	context.subscriptions.push(disposable);
+		vscode.commands.registerCommand('pubspec-dependency-inspector.analyzeDependencies', () => {
+			vscode.window.showInformationMessage('Analyzing dependencies...');
+		}),
+	);
+
+	vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
+		// Check if the opened document is the specific file you want to target
+        if (document.fileName === 'path/to/your/file') {
+            // Execute your desired command here
+            vscode.commands.executeCommand('pubspec-dependency-inspector.analyzeDependencies');
+        }
+	});
 }
 
 // This method is called when your extension is deactivated
